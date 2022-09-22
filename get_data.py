1ba5b873c81528d612a68944f8f517e170ab2c45
CerdoFunk <nahuel@icf.unam.mx>
2022-09-22 13:09:59 -0500

    Se cambió lo que la función data_to_csv devuelve para generar archivos csv que tengan todas los valores de las criptomonedas pero sólo considerando un atributo, ya sea: Adj Close, Volume, Close u Open. Sería práctico automatizar la generachión de los archivos csv tal que calculara Volume.csv, Adj_Close.csv, etc de manera autómatica. Por el momento debes cambiarlo manualmente en el código.

--- get_data.py
+++ get_data.py
@@ -7,10 +7,8 @@ from pandas_datareader import data as pdr
 from yahooquery import Screener
 
 #Fechas en formato (AAAA, M, D) usando paqueteria datetime.
-initial_t = datetime.datetime(2022, 9, 1)
+initial_t = datetime.datetime(2018, 1, 1)
 final_t = datetime.datetime(2022, 9, 9)
-#Habra que buscar una forma de obtener nombres sin que se tenga que escribir una lista.
-#raw_data = ['BTC-USD', 'GE']
 
 def get_symbols():
     """Función para obtener todos los nombres de las criptos 
@@ -22,23 +20,29 @@ def get_symbols():
     print(symbols)
     return symbols
 
-#raw_data = get_symbols()
-#print(raw_data)
 def data_to_csv(start, end, symbol):
     """Función para probar la descarga de datos de yahoo finance
-    (pero se pueden usar otras fuentes de internen. Listadas en:
+    (pero se pueden usar otras fuentes de internet. Listadas en:
     https://pandas-datareader.readthedocs.io/en/latest/remote_data.html)
     Dicha función necesita una fecha inicial y final (start y end)
     y una lista (symbol) con los nombres que se quieren descargar 
-    en dicho intervalo de tiempo."""
+    en dicho intervalo de tiempo. Al final la función devuelve
+    los precios de Adj Close para cada cripto seleccionada y lo escribe
+    en un archivo.csv"""
 
 
     #Para hacer leible la fecha y usarla en el nombre del archivo.csv de salida.
     time_i = start.strftime('%Y-%m-%d')
     time_f = end.strftime('%Y-%m-%d')
     df = pdr.DataReader(symbol, 'yahoo', start = start, end = end)
+    print(df.head())
+    print("-----------------------------")
+    print(df['Adj Close'])
 
-    return df.to_csv("from" + time_i + "to" + time_f + "of" + "allYahooCryptoSymbols.csv")
+    # La función devuelve todas los precios de Adj Close para cada cripto en un archivo.csv.
+    # Adj Close es sólo uno de los atributos de la data descargada. Modificando el return por
+    # Volume, Close, Open generara un archivo.csv pero considerando esos atributos. 
+    return df['Adj Close'].to_csv("from" + time_i + "to" + time_f + "of" + "allYahooVolumeCryptoSymbols.csv")
 
 data_to_csv(initial_t, final_t, get_symbols())
 
